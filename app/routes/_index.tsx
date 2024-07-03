ec82268281d958a32ae141980a9c3c39e3c3ab21
Arkadiusz Mazurkiewicz <mazurkiewiczar@gmail.com>
2024-07-03 09:24:44 +0200

    dedup component names

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -9,6 +9,10 @@ import { matchSorter } from 'match-sorter';
 
 const isDev = process.env.NODE_ENV === 'development';
 
+const splitCamelcase = (s: string) => s.replaceAll(/([a-z])([A-Z])/g, '$1 $2');
+const capitalizeSubsequentWords = (s: string) =>
+  s.replaceAll(/\s[a-z]/g, (match) => match.toUpperCase());
+
 export const meta: MetaFunction = () => {
   return [
     { title: 'Compare UI' },
@@ -80,113 +84,146 @@ const scrapeGithubDirectoryFileLinks = ({
     })
   );
 
+const shadCn = scrapeComponentLinks({
+  url: 'https://ui.shadcn.com/docs',
+  base: 'https://ui.shadcn.com',
+  linkSelector:
+    'body > div:nth-child(2) > div > main > div > div > aside > div > div > div > div > div:nth-child(2) > div > a',
+}).pipe(
+  Effect.map((data) => ({ ...data, name: 'Shadcn' })),
+  Effect.orElseSucceed(() => null)
+);
+
+const arkUi = scrapeComponentLinks({
+  url: 'https://ark-ui.com/react/docs/overview/introduction',
+  base: 'https://ark-ui.com',
+  linkSelector: 'aside > nav > ul > li:nth-child(3) > div > div > ul > li > a',
+}).pipe(
+  Effect.map((data) => ({ ...data, name: 'ArkUI' })),
+  Effect.orElseSucceed(() => null)
+);
+
+const zagJs = scrapeComponentLinks({
+  url: 'https://zagjs.com/overview/introduction',
+  base: 'https://zagjs.com',
+  linkSelector: 'nav > ul > li:nth-child(2) > ul > li > a',
+}).pipe(
+  Effect.map((data) => ({ ...data, name: 'ZagJS' })),
+  Effect.orElseSucceed(() => null)
+);
+
+const nextUi = scrapeComponentLinks({
+  url: 'https://nextui.org/docs/guide/introduction',
+  base: 'https://nextui.org',
+  linkSelector:
+    '#app-container > main > div > div.hidden.overflow-visible.relative.z-10.lg\\:block.lg\\:col-span-2.mt-8.pr-4 > div > div > div > div > ul:nth-child(4) > div.flex.flex-col.gap-3.items-start > li > div > a',
+}).pipe(
+  Effect.map((data) => ({ ...data, name: 'Next UI' })),
+  Effect.orElseSucceed(() => null)
+);
+
+const baseUi = scrapeComponentLinks({
+  url: 'https://mui.com/base-ui/all-components/',
+  base: 'https://mui.com',
+  linkSelector:
+    '#__next > div > nav > div > div > div.MuiBox-root > div > ul > li:nth-child(2) > div > div > div > ul > li > ul > li > a',
+}).pipe(
+  Effect.map((data) => ({ ...data, name: 'Base UI' })),
+  Effect.orElseSucceed(() => null)
+);
+
+const arekUi = scrapeGithubDirectoryFileLinks({
+  url: 'https://github.com/arekmaz/arek-ui/tree/main/app/components/ui',
+  base: 'https://arek-ui.fly.dev/?q=',
+  linkSelector:
+    'table > tbody > tr > td.react-directory-row-name-cell-large-screen > div > div > div > div > a',
+}).pipe(
+  Effect.map(({ components, ...rest }) => ({
+    ...rest,
+    components: components.map(({ name, ...c }) => ({
+      ...c,
+      name: splitCamelcase(name),
+    })),
+  })),
+  Effect.map((data) => ({ ...data, name: 'Arek UI' })),
+  Effect.orElseSucceed(() => null)
+);
+
+const materialUi = scrapeComponentLinks({
+  url: 'https://mui.com/material-ui/all-components/',
+  base: 'https://mui.com',
+  linkSelector:
+    '#__next > div > nav > div > div > div.MuiBox-root > div > ul > li:nth-child(2) > div > div > div > ul > li > ul > li > a',
+}).pipe(
+  Effect.map(({ components, ...rest }) => ({
+    ...rest,
+    name: 'Material UI',
+    components: components.filter(
+      (component) =>
+        ![/about-the-lab/, /react-use-media-query/].some((re) =>
+          re.test(component.url)
+        )
+    ),
+  })),
+  Effect.orElseSucceed(() => null)
+);
+
+const reactAria = scrapeComponentLinks({
+  url: 'https://react-spectrum.adobe.com/react-aria/components.html',
+  base: 'https://react-spectrum.adobe.com/',
+  linkSelector: 'nav > details:nth-child(8) > ul > li > a',
+}).pipe(
+  Effect.map(({ components, ...rest }) => ({
+    ...rest,
+    components: components.map(({ name, ...c }) => ({
+      ...c,
+      name: splitCamelcase(name),
+    })),
+  })),
+  Effect.map((data) => ({ ...data, name: 'React Aria' })),
+  Effect.orElseSucceed(() => null)
+);
+
+const parkUi = scrapeComponentLinks({
+  url: 'https://park-ui.com/docs/panda/components/accordion',
+  base: 'https://park-ui.com',
+  linkSelector:
+    '#sidebar > astro-island:nth-child(1)> aside:nth-child(1) > nav:nth-child(1) > ul.d_flex > li:nth-child(4) > #collapsible\\:\\:r24R4\\: > #collapsible\\:\\:r24R4\\:\\:content > ul:nth-child(1) > li > a',
+}).pipe(
+  Effect.map((data) => ({ ...data, name: 'Park UI' })),
+  Effect.orElseSucceed(() => null)
+);
+
+const reactBootstrap = scrapeComponentLinks({
+  url: 'https://react-bootstrap.github.io/docs/components/accordion',
+  base: 'https://react-bootstrap.github.io',
+  linkSelector:
+    '#__docusaurus_skipToContent_fallback > div > div > aside > div > div > nav > ul > li:nth-child(4) > ul > li > a',
+}).pipe(
+  Effect.map((data) => ({
+    ...data,
+    name: 'React Boostrap',
+    components: data.components.map(({ name, ...cmp }) => ({
+      ...cmp,
+      name: capitalizeSubsequentWords(name.replace(/s$/, '')),
+    })),
+  })),
+  Effect.orElseSucceed(() => null)
+);
+
 export const loader = loaderFunction(
   Effect.gen(function* () {
     const collectionEffects = [
-      scrapeComponentLinks({
-        url: 'https://ui.shadcn.com/docs',
-        base: 'https://ui.shadcn.com',
-        linkSelector:
-          'body > div:nth-child(2) > div > main > div > div > aside > div > div > div > div > div:nth-child(2) > div > a',
-      }).pipe(
-        Effect.map((data) => ({ ...data, name: 'Shadcn' })),
-        Effect.orElseSucceed(() => null)
-      ),
-      scrapeComponentLinks({
-        url: 'https://ark-ui.com/react/docs/overview/introduction',
-        base: 'https://ark-ui.com',
-        linkSelector:
-          'aside > nav > ul > li:nth-child(3) > div > div > ul > li > a',
-      }).pipe(
-        Effect.map((data) => ({ ...data, name: 'ArkUI' })),
-        Effect.orElseSucceed(() => null)
-      ),
-      scrapeComponentLinks({
-        url: 'https://zagjs.com/overview/introduction',
-        base: 'https://zagjs.com',
-        linkSelector: 'nav > ul > li:nth-child(2) > ul > li > a',
-      }).pipe(
-        Effect.map((data) => ({ ...data, name: 'ZagJS' })),
-        Effect.orElseSucceed(() => null)
-      ),
-      scrapeComponentLinks({
-        url: 'https://nextui.org/docs/guide/introduction',
-        base: 'https://nextui.org',
-        linkSelector:
-          '#app-container > main > div > div.hidden.overflow-visible.relative.z-10.lg\\:block.lg\\:col-span-2.mt-8.pr-4 > div > div > div > div > ul:nth-child(4) > div.flex.flex-col.gap-3.items-start > li > div > a',
-      }).pipe(
-        Effect.map((data) => ({ ...data, name: 'Next UI' })),
-        Effect.orElseSucceed(() => null)
-      ),
-      scrapeComponentLinks({
-        url: 'https://mui.com/base-ui/all-components/',
-        base: 'https://mui.com',
-        linkSelector:
-          '#__next > div > nav > div > div > div.MuiBox-root > div > ul > li:nth-child(2) > div > div > div > ul > li > ul > li > a',
-      }).pipe(
-        Effect.map((data) => ({ ...data, name: 'Base UI' })),
-        Effect.orElseSucceed(() => null)
-      ),
-      scrapeGithubDirectoryFileLinks({
-        url: 'https://github.com/arekmaz/arek-ui/tree/main/app/components/ui',
-        base: 'https://arek-ui.fly.dev/?q=',
-        linkSelector:
-          'table > tbody > tr > td.react-directory-row-name-cell-large-screen > div > div > div > div > a',
-      }).pipe(
-        Effect.map((data) => ({ ...data, name: 'Arek UI' })),
-        Effect.orElseSucceed(() => null)
-      ),
-      scrapeComponentLinks({
-        url: 'https://mui.com/material-ui/all-components/',
-        base: 'https://mui.com',
-        linkSelector:
-          '#__next > div > nav > div > div > div.MuiBox-root > div > ul > li:nth-child(2) > div > div > div > ul > li > ul > li > a',
-      }).pipe(
-        Effect.map(({ components, ...rest }) => ({
-          ...rest,
-          name: 'Material UI',
-          components: components.filter(
-            (component) =>
-              ![/about-the-lab/, /react-use-media-query/].some((re) =>
-                re.test(component.url)
-              )
-          ),
-        })),
-        Effect.orElseSucceed(() => null)
-      ),
-      scrapeComponentLinks({
-        url: 'https://react-spectrum.adobe.com/react-aria/components.html',
-        base: 'https://react-spectrum.adobe.com/',
-        linkSelector: 'nav > details:nth-child(8) > ul > li > a',
-      }).pipe(
-        Effect.map((data) => ({ ...data, name: 'React Aria' })),
-        Effect.orElseSucceed(() => null)
-      ),
-      scrapeComponentLinks({
-        url: 'https://park-ui.com/docs/panda/components/accordion',
-        base: 'https://park-ui.com',
-        linkSelector:
-          '#sidebar > astro-island:nth-child(1)> aside:nth-child(1) > nav:nth-child(1) > ul.d_flex > li:nth-child(4) > #collapsible\\:\\:r24R4\\: > #collapsible\\:\\:r24R4\\:\\:content > ul:nth-child(1) > li > a',
-      }).pipe(
-        Effect.map((data) => ({ ...data, name: 'Park UI' })),
-        Effect.orElseSucceed(() => null)
-      ),
-      scrapeComponentLinks({
-        url: 'https://react-bootstrap.github.io/docs/components/accordion',
-        base: 'https://react-bootstrap.github.io',
-        linkSelector:
-          '#__docusaurus_skipToContent_fallback > div > div > aside > div > div > nav > ul > li:nth-child(4) > ul > li > a',
-      }).pipe(
-        Effect.map((data) => ({
-          ...data,
-          name: 'React Boostrap',
-          components: data.components.map(({ name, ...cmp }) => ({
-            ...cmp,
-            name: name.replace(/s$/, ''),
-          })),
-        })),
-        Effect.orElseSucceed(() => null)
-      ),
+      shadCn,
+      arkUi,
+      zagJs,
+      nextUi,
+      baseUi,
+      arekUi,
+      materialUi,
+      reactAria,
+      parkUi,
+      reactBootstrap,
     ].map(Effect.cachedWithTTL(isDev ? 0 : '24 hours'));
 
     const cachedEffects = yield* Effect.all(collectionEffects);
